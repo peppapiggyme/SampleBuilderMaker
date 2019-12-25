b8c38093b61f7ba97dbba049799e055942ff40db
Bowen <bowen@zhangbmacbook.dyndns.cern.ch>
2019-12-25 17:20:25 +0800

    add main funtion to build yields data

--- build_data.py
+++ build_data.py
@@ -1,10 +1,11 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 # encoding: utf-8
 """
-main.py
+build_data.py
 ~~~~~~~~~
 
-Main function to get yield data from Reader output
+Main function to build yield data from Reader output
+Output is the `yield.dictionary`
 
 """
 
@@ -15,6 +16,7 @@ import argparse
 
 from pprint import pprint
 from sample_builder.sbhistograms import SBHistograms
+from sample_builder.sbyields import SBYields
 from utils.logging_tools import get_logger
 
 from ROOT import gROOT
@@ -22,22 +24,74 @@ from ROOT import gROOT
 print("My ROOT version is {}".format(gROOT.GetVersion()))
 
 
-def main(args):
-    logger = get_logger("MAIN", args.msglvl)
+def build_data(args):
+    # logger
+    logger = get_logger("MAIN", "INFO")
 
+    # PRE-DEFINITIONS
+    region_prefix = "2tag2pjet_0ptv_SRLRJwindow"
+    disc = "subsmhh"
+    signal_prefix = "Hhhbbtautau"
+    masses = [1000, 1200, 1400, 1600, 1800, 2000, 2500, 3000]
+    binning = [0., 4000.]
+
+    # histograms data
+    logger.info("# 1 - Histograms")
+    sbh = SBHistograms(args.input, region_prefix, masses)
+    sbh.disc = disc
+    sbh.signal_prefix = signal_prefix
+    sbh.masscut = {
+        "1000": "",
+        "1200": "",
+        "1400": "",
+        "1600": "MHH900",
+        "1800": "MHH900",
+        "2000": "MHH900",
+        "2500": "MHH1200",
+        "3000": "MHH1200",
+    }
+    sbh.save_data(args.histograms)
+
+    # yields data
+    logger.info("# 1 - Yields")
+    sby = SBYields(args.input, region_prefix, masses, binning)
+
+    # sby.shape_systs = ['SysFATJET_Medium_JET_Comb_Baseline_Kin',
+    #                  'SysFATJET_Medium_JET_Comb_TotalStat_Kin',
+    #                  'SysFATJET_Medium_JET_Comb_Modelling_Kin',
+    #                  'SysFATJET_Medium_JET_Comb_Tracking_Kin',
+    #                  'SysFATJET_JER', 'SysFATJET_JMR',
+    #                  'SysTAUS_TRUEHADDITAU_EFF_JETID_TOTAL',
+    #                  'SysTAUS_TRUEHADDITAU_SME_TES_TOTAL',
+    #                  'SysMET_SoftTrk_ResoPerp', 'SysMET_SoftTrk_ResoPara',
+    #                  'SysMET_JetTrk_Scale', 'SysMET_SoftTrk_Scale',
+    #                  'SysPRW_DATASF', ]
+
+    sby.diboson = ['WWPw', 'WZPw', 'ZZPw']
+    sby.Wjets = ['Wbb', 'Wbc', 'Wbl', 'Wcc', 'Wcl', 'Wl']
+    sby.Zhf = ['Zbb', 'Zbc', 'Zbl', 'Zcc']
+    sby.Zlf = ['Zcl', 'Zl']
+    sby.Zee = ['ZeeSh221']
+    sby.top = ['ttbar', 'stop', 'stops', 'stopt', 'stopWt', 'ttbar_allhad', 'ttbar_nonallhad']
+    sby.for_histfitter = True
+    sby.do_merging = True
+
+    sby.disc = disc
+    sby.signal_prefix = signal_prefix
+    sby.cache_name = args.histograms
+
+    sby.save_data(args.yields)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", default="",
-                        help="Name of merged file.")
-    parser.add_argument("-i", "--input", default="", nargs='*',
-                        help="Input root file pattern, Same as those in hadd.")
-    parser.add_argument("-l", "--msglvl", default="INFO",
-                        help="Log message level.")
-    parser.add_argument("-j", "--ncpus", default=10,
-                        help="Number of parallel processes of hadd merge")
+    parser.add_argument("-i", "--input", default="root_files/submitDir_v10_mc16ade.root",
+                        help="CxAODReader output root file (merged by bohadd)")
+    parser.add_argument("--histograms", default="pickle_files/histograms.dictionary",
+                        help="Output histograms data name")
+    parser.add_argument("--yields", default="pickle_files/yields.dictionary",
+                        help="Output yields data name")
 
     args = parser.parse_args()
 
-    main(args)
+    build_data(args)
